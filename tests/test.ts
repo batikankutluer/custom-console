4ff18a83126d2c34722f65885e2ea05aabdf7d11
Batikan-Kutluer <60034174+Batikan-Kutluer@users.noreply.github.com>
2025-02-04 01:24:20 +0300

    Custom Console - V1.0.3

--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-import CustomConsole from "../source/CustomConsole.ts";
-
-const console = CustomConsole({ test: true });
-
-console.log("Hello World!", chalk.green("Huh!"));
-console.error("Error!");
-console.write("Write!");
